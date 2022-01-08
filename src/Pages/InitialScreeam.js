1c9558b76f92dad440642958e72224bc546d5448
Rafael <rafaeloliveiracds@hotmail.com>
2022-01-07 22:52:30 -0300

    att

--- src/Pages/InitialScreeam.js
+++ src/Pages/InitialScreeam.js
@@ -126,13 +126,6 @@ const InitialScream = () => {
 
   return (
     <div className="body-initial-screem">
-      <img
-        className="img-background-wordlist"
-        src={backgroundimg}
-        alt="Backgroundimage"
-        width={1920}
-        height={1080}
-      />
       <div className="div-new-list">
         <div>
           <button className="new-list" onClick={(e) => show(e)}>
