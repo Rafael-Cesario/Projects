069a4ea8a7febdc01c8f30b761b63cd82fae828a
Rafael <rafaeloliveiracds@hotmail.com>
2022-02-27 15:21:31 -0300

    att

--- src/Components/QuestionScreen.js
+++ src/Components/QuestionScreen.js
@@ -1,132 +1,117 @@
-import {
-  animationNextQuestion,
-  animationCheckAnswer,
-  next02FindAnimation,
-} from "../gsap/animations";
+import { animationNextQuestion, animationCheckAnswer, next02FindAnimation } from "../gsap/animations";
 import { WordListStore } from "../context/WordListStore";
 import { useContext } from "react";
 import { useState } from "react";
 
 const QuestionScreen = ({ study, setScreen01, params }) => {
-  const { wordListStore } = useContext(WordListStore);
-  const inputAnswer = document.querySelector(".input-answer");
-  const [whichFuncCall, setWhichFuncCall] = useState("checkAnswer");
-  const [inputAnswerValue, setInputAnswerValue] = useState("");
-  const [indexWord, setIndexWord] = useState(0);
-  const [changeToRightAnswer, setChangeToRightAnswer] = useState(false);
-  const [isRigthAnswer, setIsRightAnswer] = useState(false);
+	const { wordListStore } = useContext(WordListStore);
+	const inputAnswer = document.querySelector(".input-answer");
+	const [whichFuncCall, setWhichFuncCall] = useState("checkAnswer");
+	const [inputAnswerValue, setInputAnswerValue] = useState("");
+	const [indexWord, setIndexWord] = useState(0);
+	const [changeToRightAnswer, setChangeToRightAnswer] = useState(false);
+	const [isRigthAnswer, setIsRightAnswer] = useState(false);
 
-  const answerWith = [
-    wordListStore[params.id].individualWordList[params.index].answerWith,
-    wordListStore[params.id].individualWordList[params.index].answerWith ===
-    "Definition"
-      ? "Term"
-      : "Definition",
-  ];
+	const answerWith = [
+		wordListStore[params.id].individualWordList[params.index].answerWith,
+		wordListStore[params.id].individualWordList[params.index].answerWith === "Definition" ? "Term" : "Definition",
+	];
 
-  const next = (e) => {
-    e.preventDefault();
-    switch (whichFuncCall) {
-      case "checkAnswer":
-        finding();
-        animationCheckAnswer(finding());
-        setWhichFuncCall("NextQuestion");
-        break;
+	const next = (e) => {
+		e.preventDefault();
+		switch (whichFuncCall) {
+			case "checkAnswer":
+				finding();
+				animationCheckAnswer(finding());
+				setWhichFuncCall("NextQuestion");
+				break;
 
-      case "NextQuestion":
-        animationNextQuestion(nextQuestion, e, isRigthAnswer);
-        setWhichFuncCall("checkAnswer");
-        break;
+			case "NextQuestion":
+				animationNextQuestion(nextQuestion, e, isRigthAnswer);
+				setWhichFuncCall("checkAnswer");
+				break;
 
-      default:
-        console.log("erro");
-        return;
-    }
-  };
+			default:
+				console.log("erro");
+				return;
+		}
+	};
 
-  const nextQuestion = (e, isRigthAnswer) => {
-    if (isRigthAnswer === true) {
-      next02FindAnimation();
-      study.studying.splice(indexWord, 1);
+	const nextQuestion = (e, isRigthAnswer) => {
+		if (isRigthAnswer === true) {
+			next02FindAnimation();
+			study.studying.splice(indexWord, 1);
 
-      if (study.toStudy.length > 0) {
-        study.studying.push({
-          Term: study.toStudy[0][0],
-          Definition: study.toStudy[0][1],
-        });
-        study.toStudy.shift();
-      }
-    }
+			if (study.toStudy.length > 0) {
+				study.studying.push({
+					Term: study.toStudy[0][0],
+					Definition: study.toStudy[0][1],
+				});
+				study.toStudy.shift();
+			}
+		}
 
-    if (study.studying.length === 0) {
-      setScreen01(false);
-      return;
-    }
+		if (study.studying.length === 0) {
+			setScreen01(false);
+			return;
+		}
 
-    indexWord === study.studying.length - 1 || study.studying.length === 1
-      ? setIndexWord(0)
-      : isRigthAnswer === true
-        ? setIndexWord(indexWord)
-        : setIndexWord(indexWord + 1);
-         
+		if (isRigthAnswer === false) {
+			setIndexWord(indexWord + 1);
+		}
 
-    setChangeToRightAnswer(false);
-    setInputAnswerValue("");
-    inputAnswer.focus();    
-  };
+		if (indexWord === study.studying.length - 1 || study.studying.length === 1) {
+			setIndexWord(0);
+		}
 
-  const finding = () => {
-    let word01 = study.studying[indexWord][answerWith[0]].trim().toUpperCase();
-    let word02 = inputAnswerValue.trim().toUpperCase();
+		setChangeToRightAnswer(false);
+		setInputAnswerValue("");
+		inputAnswer.focus();
+	};
 
-    if (word01 === word02 || word01.split(", ").indexOf(word02) > -1) {
-      setIsRightAnswer(true);
-      return true;
-    }
+	const finding = () => {
+		let word01 = study.studying[indexWord][answerWith[0]].trim().toUpperCase();
+		let word02 = inputAnswerValue.trim().toUpperCase();
 
-    setIsRightAnswer(false);
-    setChangeToRightAnswer(true);
-    return false;
-  };
+		if (word01 === word02 || word01.split(", ").indexOf(word02) > -1) {
+			setIsRightAnswer(true);
+			return true;
+		}
 
-  const markAnswerAsRight = (e) => {
-    const find = true;
-    animationNextQuestion(nextQuestion, e, find);
-    setWhichFuncCall("checkAnswer");
-  };
+		setIsRightAnswer(false);
+		setChangeToRightAnswer(true);
+		return false;
+	};
 
-  return (
-    <div className="container">
-      <h2 className="word01">{study.studying[indexWord][answerWith[1]]}</h2>
+	const markAnswerAsRight = (e) => {
+		const find = true;
+		animationNextQuestion(nextQuestion, e, find);
+		setWhichFuncCall("checkAnswer");
+	};
 
-      <h2 className="word02">{study.studying[indexWord][answerWith[0]]}</h2>
+	return (
+		<div className="container">
+			<h2 className="word01">{study.studying[indexWord][answerWith[1]]}</h2>
 
-      {changeToRightAnswer && (
-        <button type="button" onClick={(e) => markAnswerAsRight(e)}>
-          Marcar resposta como correta
-        </button>
-      )}
+			<h2 className="word02">{study.studying[indexWord][answerWith[0]]}</h2>
 
-      <form onSubmit={(e) => next(e)} className="form-answer">
-        <input
-          type="text"
-          placeholder="Resposta"
-          value={inputAnswerValue}
-          onChange={(e) => setInputAnswerValue(e.target.value)}
-          className="input-answer"
-        />
-        <div className="buttons">
-          <button>Confirmar</button>
-          <button>Não sei</button>
-        </div>
-      </form>
+			{changeToRightAnswer && (
+				<button type="button" onClick={(e) => markAnswerAsRight(e)}>
+					Marcar resposta como correta
+				</button>
+			)}
 
-      <p className="remain-words">
-        {study.toStudy.length + study.studying.length} Palavras até terminar a
-        lista{" "}
-      </p>
-    </div>
-  );
+			<form onSubmit={(e) => next(e)} className="form-answer">
+				<input type="text" placeholder="Resposta" value={inputAnswerValue} onChange={(e) => setInputAnswerValue(e.target.value)} className="input-answer" />
+				<div className="buttons">
+					<button>Confirmar</button>
+					<button>Não sei</button>
+				</div>
+			</form>
+
+			<p className="remain-words">{study.toStudy.length + study.studying.length} Palavras até terminar a lista </p>
+		</div>
+	);
 };
 
 export default QuestionScreen;
