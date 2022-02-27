0e2b5fe07c423ecd81e33446040391852b4b8595
Rafael <rafaeloliveiracds@hotmail.com>
2022-02-27 16:41:54 -0300

    att

--- src/context/WordListStore.js
+++ src/context/WordListStore.js
@@ -7,20 +7,16 @@ export const WordListStore = createContext();
 const myWordReducer = produce(wordListReducer);
 
 const WordListStoreProvider = ({ children }) => {
-  const [wordListStore, dispatch] = useReducer(myWordReducer, {}, () => {
-    const data = localStorage.getItem("wordListStore");
-    return data ? JSON.parse(data) : {};
-  });
+	const [wordListStore, dispatch] = useReducer(myWordReducer, {}, () => {
+		const data = localStorage.getItem("wordListStore");
+		return data ? JSON.parse(data) : {};
+	});
 
-  useEffect(() => {
-    localStorage.setItem("wordListStore", JSON.stringify(wordListStore));
-  }, [wordListStore]);
+	useEffect(() => {
+		localStorage.setItem("wordListStore", JSON.stringify(wordListStore));
+	}, [wordListStore]);
 
-  return (
-    <WordListStore.Provider value={{ wordListStore, dispatch }}>
-      {children}
-    </WordListStore.Provider>
-  );
+	return <WordListStore.Provider value={{ wordListStore, dispatch }}>{children}</WordListStore.Provider>;
 };
 
 export default WordListStoreProvider;
