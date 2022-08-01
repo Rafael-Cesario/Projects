25445702673b71e517f850f6fbb973eabb47dca1
Rafael <rafaeloliveiracds@hotmail.com>
2022-08-01 17:01:33 -0300

    refactor: new max-width for month component

--- frontend/src/styles/monthStyle.tsx
+++ frontend/src/styles/monthStyle.tsx
@@ -2,66 +2,67 @@ import styled from 'styled-components';
 import { palette } from './globalStyle';
 
 export const MonthStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 140px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	height: 140px;
 
-  button {
-    border: none;
-    outline: none;
-    background-color: transparent;
-    color: white;
-    font-family: ${palette.fontFamily};
-    cursor: pointer;
+	button {
+		border: none;
+		outline: none;
+		background-color: transparent;
+		color: white;
+		font-family: ${palette.fontFamily};
+		cursor: pointer;
 
-    ::before {
-      content: attr(data-name);
-      position: absolute;
-      transform: translate(-41px, -40px);
-      visibility: hidden;
-      pointer-events: none;
-      background-color: ${palette.mainBlack};
-      padding: 1px 10px;
-      border-radius: ${palette.borderRadius};
-    }
+		::before {
+			content: attr(data-name);
+			position: absolute;
+			transform: translate(-41px, -40px);
+			visibility: hidden;
+			pointer-events: none;
+			background-color: ${palette.mainBlack};
+			padding: 1px 10px;
+			border-radius: ${palette.borderRadius};
+		}
 
-    :hover {
-      ::before {
-        visibility: visible;
-      }
-    }
-  }
+		:hover {
+			::before {
+				visibility: visible;
+			}
+		}
+	}
 
-  .year {
-    display: flex;
+	.year {
+		display: flex;
 
-    .title {
-      margin: 2rem;
-    }
-  }
+		.title {
+			margin: 2rem;
+		}
+	}
 
-  .days {
-    display: flex;
-    flex-wrap: wrap;
-    width: 80vw;
-  }
+	.days {
+		display: flex;
+		flex-wrap: wrap;
+		width: 80vw;
+    max-width: 800px;
+	}
 
-  .day {
-    width: 15px;
-    height: 15px;
-    margin: 2px;
-    background-color: ${palette.mainBlack};
-  }
+	.day {
+		width: 15px;
+		height: 15px;
+		margin: 2px;
+		background-color: ${palette.mainBlack};
+	}
 
-  .active {
-    background-color: ${palette.mainBlue};
-  }
+	.active {
+		background-color: ${palette.mainBlue};
+	}
 
-  .no-name {
-    color: ${palette.mainWhite + 'aa'};
-    font-weight: normal;
-    letter-spacing: 1px;
-    font-size: 1rem;
-  }
+	.no-name {
+		color: ${palette.mainWhite + 'aa'};
+		font-weight: normal;
+		letter-spacing: 1px;
+		font-size: 1rem;
+	}
 `;
