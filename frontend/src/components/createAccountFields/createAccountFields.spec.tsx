94f3a22a80c4fa5d28be9fa3ed192072960bf3a3
Rafael <rafaeloliveiracds@hotmail.com>
2022-08-17 14:57:53 -0300

    Test: Account Form, Password input type

--- frontend/src/components/createAccountFields/createAccountFields.spec.tsx
+++ frontend/src/components/createAccountFields/createAccountFields.spec.tsx
@@ -1,3 +1,20 @@
+import { fireEvent, render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
+import { CreateAccountFields } from './createAccountFields';
 
-describe.todo('Create Account Form');
+describe('Create Account Form', () => {
+	it('Change the password input type', () => {
+		const { getAllByLabelText, getAllByText } = render(<CreateAccountFields />);
+		const passwordInput = getAllByLabelText(/senha/i)[0];
+		const confirmPasswordInput = getAllByLabelText(/senha/i)[1];
+
+		const showPasswordButton = getAllByText(/mostrar senha/i)[0];
+
+		expect(passwordInput.getAttribute('type')).toBe('password');
+		expect(confirmPasswordInput.getAttribute('type')).toBe('password');
+
+		fireEvent.click(showPasswordButton);
+
+		expect(confirmPasswordInput.getAttribute('type')).toBe('text');
+	});
+});
