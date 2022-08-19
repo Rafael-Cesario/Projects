88513eb8bb662f30a036d81b71c03029e8c36b1d
Rafael <rafaeloliveiracds@hotmail.com>
2022-08-18 23:21:48 -0300

    Feature: Verify Email

--- frontend/src/components/createAccount/utils/verifyFields.ts
+++ frontend/src/components/createAccount/utils/verifyFields.ts
@@ -1,12 +1,30 @@
-const verify = (entries: [string, string | null][]) => {
-	const errorMessage = [];
-	entries.forEach(([key, value]) => value || errorMessage.push(key));
-	return errorMessage;
-};
+class Verify {
+	empty(entries: [string, string | null][]) {
+		const errorMessage = [];
+		entries.forEach(([key, value]) => value || errorMessage.push(key));
+		return errorMessage;
+	}
+
+	email(email: string) {
+		const hasSign = email.includes('@'); // verify if the email includes @
+		const endsWith = email.endsWith('.com'); // verify if ends with .com
+		const startsWithText = email.search(/^[a-z]/) > -1 ? true : false; // verify if starts with letters
+		const hasTextAfterSign = email.search(/@[a-z]/) > -1 ? true : false; // verify if has text afeter the sign
+
+		const isValid = [hasSign, endsWith, startsWithText, hasTextAfterSign].includes(false);
+		return !isValid;
+	}
+}
 
 export const verifyFields = (fields: Record<string, string>) => {
+	const verify = new Verify();
 	const entries = Object.entries(fields);
-	const emptyFields = verify(entries);
 
-	return emptyFields.length > 0 ? emptyFields : false;
+	const emptyFields = verify.empty(entries);
+	const isEmailValid = verify.email(fields.email);
+
+	if (emptyFields.length > 0) return emptyFields;
+	if (!isEmailValid) return isEmailValid;
+
+	return false;
 };
