48c4e4083a4357c5a92ce8e4456341ceb59a29a9
Rafael <rafaeloliveiracds@gmail.com>
2022-11-23 21:12:04 -0300

    Calculator [refactor]: Keyboard JSX

--- Calculator/src/components/keyboard/keyboard.tsx
+++ Calculator/src/components/keyboard/keyboard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CalculatorClass } from '../../utils/calculatorClass';
 import { Operator } from '../../utils/types';
 import { IKeyboard } from './KeyboardInterface';
@@ -5,27 +6,19 @@ import { KeyboardStyle } from './KeyboardStyle';
 
 export const Keyboard = ({ props }: IKeyboard) => {
 	const { screen, setScreen, preview, setPreview } = props;
-	const Calculator = new CalculatorClass(screen, setScreen, preview, setPreview);
+	const [operator, setOperator] = useState<Operator>('+');
+	const [value, setValue] = useState(0);
 
-	const keys = [
-		['%', '/'],
-		[7, 8, 9, 'x'],
-		[4, 5, 6, '-'],
-		[1, 2, 3, '+'],
-		['+/-', 0],
-	];
+	const Calculator = new CalculatorClass(screen, setScreen, preview, setPreview, operator, setOperator, value, setValue);
 
 	const addKey = (e: React.SyntheticEvent) => {
 		const button = e.target as HTMLButtonElement;
 		const text = button.textContent as string;
 		const isOperator = !!button.className.match(/operators/);
 
-		if (!isOperator) {
-			setScreen(screen + text);
-			return;
-		}
+		if (!isOperator) return setScreen(screen + text);
 
-		Calculator.setOperator(text as Operator);
+		Calculator.next(text as Operator, Number(screen));
 	};
 
 	return (
@@ -33,26 +26,63 @@ export const Keyboard = ({ props }: IKeyboard) => {
 			<button onClick={() => Calculator.removeLastDigit()} className='keyboard-keys'>
 				c
 			</button>
-
 			<button onClick={() => Calculator.clearScreen()} className='keyboard-keys'>
 				ca
 			</button>
-
-			{keys.map((row) =>
-				row.map((key, index) => {
-					let newClass = 'keyboard-keys';
-					if (typeof key === 'string') newClass += ' operators';
-
-					return (
-						<button onClick={(e) => addKey(e)} className={newClass} key={index}>
-							{key}
-						</button>
-					);
-				})
-			)}
-
-			<button className='keyboard-keys operators'>,</button>
-			<button className='keyboard-keys operators'>=</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys operators'>
+				%
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys operators'>
+				/
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				7
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				8
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				9
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys operators'>
+				x
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				4
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				5
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				6
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys operators'>
+				-
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				1
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				2
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				3
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys operators'>
+				+
+			</button>
+			<button onClick={() => Calculator.invert(Number(screen))} className='keyboard-keys operators'>
+				+/-
+			</button>
+			<button onClick={(e) => addKey(e)} className='keyboard-keys'>
+				0
+			</button>
+			<button onClick={() => Calculator.dot(Number(screen))} className='keyboard-keys operators'>
+				,
+			</button>
+			<button onClick={() => Calculator.result(Number(screen))} className='keyboard-keys operators'>
+				=
+			</button>
 		</KeyboardStyle>
 	);
 };
