3a49b413b2e55e2b33ba2a757018e764a047bbb4
Rafael <rafaeloliveiracds@gmail.com>
2022-11-30 16:35:07 -0300

    To do list [test]: Component lists test

--- ToDoList/components/pages/index/__tests/lists.spec.tsx
+++ ToDoList/components/pages/index/__tests/lists.spec.tsx
@@ -1,4 +1,13 @@
 import { describe, it, expect } from 'vitest';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { Lists } from '../components/lists';
 
-describe.todo('Lists Component', () => {});
+describe('Lists Component', () => {
+	it('Renders the lists', () => {
+		const lists = ['test1', 'teste2'];
+		const { queryByText } = render(<Lists props={{ lists }} />);
+
+		const list1 = queryByText(lists[0]);
+		expect(list1).not.toBeNull();
+	});
+});
