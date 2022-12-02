c03d2229e29053afd51fa0ee195b816a04343ab4
Rafael <rafaeloliveiracds@gmail.com>
2022-12-02 18:45:43 -0300

    SNotes [feat]: keyboar shortcut

--- SNotes/components/menu/menu.tsx
+++ SNotes/components/menu/menu.tsx
@@ -5,16 +5,39 @@ import { theme } from '../../styles/styledTheme';
 // Icons
 import { TiArrowUpThick, TiArrowDownThick } from 'react-icons/Ti';
 import { TbAlignLeft, TbAlignCenter, TbAlignRight } from 'react-icons/tb';
+import { useCallback, useEffect } from 'react';
 
 export const Menu = ({ props }: InterfaceMenu) => {
 	const { themeName } = props;
+	const formatText = (format: string) => document.execCommand(format);
+
+	const execShortcut = useCallback((e: KeyboardEvent) => {
+		const isAltPressed = e.altKey;
+		const key = e.key;
+
+		if (!isAltPressed) return;
+		e.preventDefault();
+
+		if (key == 'q') formatText('bold');
+		if (key == 'w') formatText('italic');
+		if (key == 'e') formatText('underline');
+	}, []);
+
+	useEffect(() => {
+		window.addEventListener('keydown', execShortcut);
+
+		return () => {
+			window.addEventListener('keydown', execShortcut);
+		};
+	}, []);
+
 	return (
 		<>
 			<StyledMenu theme={theme[themeName]}>
 				<div className='font-style'>
-					<button>N</button>
-					<button>I</button>
-					<button>S</button>
+					<button onClick={() => formatText('bold')}>N</button>
+					<button onClick={() => formatText('italic')}>I</button>
+					<button onClick={() => formatText('underline')}>S</button>
 				</div>
 
 				<div className='font-size'>
--- SNotes/components/textContainer/textContainer.tsx
+++ SNotes/components/textContainer/textContainer.tsx
@@ -1,9 +1,11 @@
 import { StyledTextContainer } from './styledTextContainer';
 import { theme } from '../../styles/styledTheme';
 import { InterfaceTextContainer } from './interfaceTextContainer';
+import { useState } from 'react';
 
 export const TextContainer = ({ props }: InterfaceTextContainer) => {
 	const { themeName } = props;
+
 	return (
 		<StyledTextContainer theme={theme[themeName]}>
 			<div className='text' contentEditable='true'></div>
