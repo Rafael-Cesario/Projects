9de37facb147ce6031fb8f2a618bb8ae7ad98fc7
Rafael <rafaeloliveiracds@gmail.com>
2022-12-04 21:46:54 -0300

    User crud restful [feat]: User Route, create , read, update, delete

--- 04-user-crud-restful/src/routes/userRoute.ts
+++ 04-user-crud-restful/src/routes/userRoute.ts
@@ -4,15 +4,39 @@ import { UserModel } from '../schema/userSchema';
 
 export const router = express.Router();
 
-// Creates a new User
 router.post('/', async (req: Request, res: Response) => {
 	const { email, password, name, age } = req.body as IUser;
 
-	if (!email) res.status(400).json({ error: 'Email is required' });
+	if (!email) return res.status(400).json({ error: 'Email is required' });
 
-	const hasUser = await UserModel.find({ email });
-	if (hasUser) res.status(400).json({ error: 'Email already exist' });
+	const users = await UserModel.find({ email });
+	const hasUser = users.length > 0;
+	if (hasUser) return res.status(400).json({ error: 'Email already exist' });
 
 	await UserModel.create({ email, password, name, age });
-	res.json({ message: 'A new User was created' });
+	res.status(200).json({ message: 'A new User was created' });
+});
+
+router.get('/', async (req: Request, res: Response) => {
+	const users = await UserModel.find({});
+	res.status(200).json({ users });
+});
+
+router.patch('/', async (req: Request, res: Response) => {
+	const { email, update } = req.body as { email: string; update: {} };
+
+	if (!email) return res.status(400).json({ error: 'Email is required' });
+	if (!update) return res.status(400).json({ error: 'A update OBJ is required' });
+
+	await UserModel.findOneAndUpdate({ email }, { ...update });
+	res.status(200).json({ message: `User ${email} was updated` });
+});
+
+router.delete('/', async (req: Request, res: Response) => {
+	const { email } = req.body as { email: string };
+
+	if (!email) return res.status(400).json({ error: 'Email is required' });
+
+	await UserModel.findOneAndDelete({ email });
+	res.status(200).json({ message: `User ${email} was deleted` });
 });
