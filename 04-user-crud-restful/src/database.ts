8b816fd3b1a59edb2265cc515dffb73c875fc1be
Rafael <rafaeloliveiracds@gmail.com>
2022-12-04 23:22:55 -0300

    user crud restful[env]: .env for test environment

--- 04-user-crud-restful/src/database.ts
+++ 04-user-crud-restful/src/database.ts
@@ -1,3 +1,6 @@
 import 'dotenv/config';
 
-export const uri = `mongodb://localhost:27017/${process.env.DATABASE}`;
+let database = process.env.DATABASE;
+if (process.env.NODE_ENV === 'test') database = 'test';
+
+export const uri = `mongodb://localhost:27017/${database}`;
