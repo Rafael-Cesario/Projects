8db5c250a1df623e1f5de5fbebbc3a4d2d803a12
Rafael <rafaeloliveiracds@gmail.com>
2022-12-13 09:19:59 -0300

    Graphql tasks [feat]: format error

--- 05-tasks-crud-graphql/src/server.ts
+++ 05-tasks-crud-graphql/src/server.ts
@@ -2,7 +2,11 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { schema } from './schemas/schema';
 
-export const server = new ApolloServer({ schema });
+export const server = new ApolloServer({
+	schema,
+	includeStacktraceInErrorResponses: false,
+	formatError: formattedError => ({ message: 'Error: ' + formattedError.message }),
+});
 
 export const startServer = async () => {
 	const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
