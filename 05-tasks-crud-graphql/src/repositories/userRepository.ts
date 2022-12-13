22ce57da5cd8f7201250afe3ea5a10f5b6fe256a
Rafael <rafaeloliveiracds@gmail.com>
2022-12-13 08:10:56 -0300

    Graphql tasks [feat]: user repository, get user, handle Error

--- 05-tasks-crud-graphql/src/repositories/userRepository.ts
+++ 05-tasks-crud-graphql/src/repositories/userRepository.ts
@@ -1,17 +1,27 @@
 import { UserModel } from '../models/userModel';
 import { TypeUser } from '../schemas/types/userType';
 
-class UserRepository {
+interface InterfaceUserRepository {
+	createUser: (userData: TypeUser) => Promise<Boolean | string>;
+	getUser: (email: string) => Promise<TypeUser>;
+}
+
+const handleError = (error: any) => {
+	if (error.code === 11000) return 'This email is already in use';
+	return error.message;
+};
+
+class UserRepository implements InterfaceUserRepository {
 	constructor(private userModel = UserModel) {}
 
 	async createUser(userData: TypeUser) {
-		try {
-			await this.userModel.create(userData);
-			return false;
-		} catch (error: any) {
-			if (error.code === 11000) return 'This email is already in use';
-			return error.message;
-		}
+		const user = await this.userModel.create(userData).catch(error => handleError(error));
+		return false;
+	}
+
+	async getUser(email: string) {
+		const user = await this.userModel.findOne({ email }).catch(error => handleError(error));
+		return user;
 	}
 }
 
