7e3704eef0a453095a5e1ac7339b6f0d2b1ff840
Rafael <rafaeloliveiracds@gmail.com>
2022-12-15 06:15:30 -0300

    Graphql authentication [ feat ]: Save user in DB

--- 06-graphql-authentication/src/models/userModel.ts
+++ 06-graphql-authentication/src/models/userModel.ts
@@ -1,10 +1,11 @@
-import { prop, getModelForClass } from '@typegoose/typegoose';
+import { prop, getModelForClass, index } from '@typegoose/typegoose';
 import { pre } from '@typegoose/typegoose/lib/hooks';
 
 @pre<UserSchema>('save', function () {
 	this.email = this.email.toUpperCase();
 	this.password = 'SECRET';
 })
+@index({ email: 1 }, { unique: true })
 class UserSchema {
 	@prop({ type: String, required: [true, 'Name is required'] })
 	public name!: string;
--- 06-graphql-authentication/src/repositories/userRepository.ts
+++ 06-graphql-authentication/src/repositories/userRepository.ts
@@ -1,6 +1,7 @@
+import { UserModel } from '../models/userModel';
 import { UserType } from '../schemas/types/userType';
 
-type Response = Promise<{ message: string; error: string }>;
+type Response = Promise<{ message: string; error: any }>;
 
 interface UserRepositoryInterface {
 	createUser: (user: UserType) => Response;
@@ -8,7 +9,12 @@ interface UserRepositoryInterface {
 
 class UserRepository implements UserRepositoryInterface {
 	async createUser(user: UserType) {
-		return { message: 'New User created', error: '' };
+		try {
+			await UserModel.create({ ...user });
+			return { message: 'New User created', error: '' };
+		} catch (error) {
+			return { message: '', error: error };
+		}
 	}
 }
 
