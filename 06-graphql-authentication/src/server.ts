5e0eccdfe7babd030a3a96e4fc50badd7ada29b9
Rafael <rafaeloliveiracds@gmail.com>
2022-12-15 07:12:43 -0300

    Graphql authentication [ feat ]: Formatted Error

--- 06-graphql-authentication/src/server.ts
+++ 06-graphql-authentication/src/server.ts
@@ -2,7 +2,11 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { schema } from './schemas/schema';
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({
+	schema,
+	includeStacktraceInErrorResponses: false,
+	formatError: formattedError => ({ message: formattedError.message }),
+});
 
 const startServer = async () => {
 	const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
