8928752f0bc1624df8cbae0ae95a56cdd667a161
Rafael <rafaeloliveiracds@gmail.com>
2022-12-15 11:10:25 -0300

    Graphql authentication [ feat ]: verifyToken

--- 06-graphql-authentication/src/utils/generateToken.ts
+++ 06-graphql-authentication/src/utils/generateToken.ts
@@ -1,8 +1,18 @@
 import 'dotenv/config';
 import jwt from 'jsonwebtoken';
 
-export const generateToken = () => {
+export const generateToken = (email: string) => {
 	const SECRET = process.env.SECRET!;
-	const token = jwt.sign({}, SECRET);
+	const expiresIn = 20;
+	const token = jwt.sign({ email }, SECRET, { expiresIn });
 	return token;
+};
+
+export const verifyToken = (token: string) => {
+	try {
+		const decoded = jwt.verify(token, process.env.SECRET!);
+		return decoded;
+	} catch (error: any) {
+		return error.message;
+	}
 };
