7214a8fda370091fe843205dcff096426a6a741b
Rafael <rafaeloliveiracds@gmail.com>
2022-12-16 11:46:02 -0300

    Recipes [ feat ]: New next project , eslint

---
+++ 07-recipes/README.md
@@ -0,0 +1,2 @@
+# Recipes app
+Made with tailwind.
---
+++ 07-recipes/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => {
+	return (
+		<>
+			<Head>
+				<title>Recipes App</title>
+			</Head>
+
+			<h1>Hello World</h1>
+		</>
+	);
+};
+
+export default Home;
