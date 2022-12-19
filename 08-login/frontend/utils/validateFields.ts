4904f25a0d0f4c548226c2018f678dfe87773b5d
Rafael <rafaeloliveiracds@gmail.com>
2022-12-19 12:01:39 -0300

    [ feat ] Login page: validate fields (password)

--- 08-login/frontend/utils/validateFields.ts
+++ 08-login/frontend/utils/validateFields.ts
@@ -3,13 +3,28 @@ type TypeValidade = 'email' | 'name' | 'password' | 'confirmPassword';
 export const validateFields = (values: { [operationName: string]: { [field: string]: string } }, active: string) => {
 	const validate = {
 		email: () => {},
+
 		name: () => {
 			const { name } = values[active];
 			const nameTooBig = name.length > 20;
 
 			if (nameTooBig) return ['name', 'Escolha um nome menor, maximo 20 dígitos'];
 		},
-		password: () => {},
+
+		password: () => {
+			const { password } = values[active];
+
+			const hasUpperCaseLetter = password.match(/[A-Z]/);
+			const hasLowerCaseLetter = password.match(/[a-z]/);
+
+			if (!hasLowerCaseLetter || !hasUpperCaseLetter) {
+				return ['password', 'Sua senha precisa ter no mínimo 1 letra maiuscula e 1 letra minuscula'];
+			}
+
+			const tooShort = password.length < 10;
+			if (tooShort) return ['password', 'Sua senha é muito curta, mínimo de 10 dígitos'];
+		},
+
 		confirmPassword: () => {},
 	};
 
@@ -21,5 +36,6 @@ export const validateFields = (values: { [operationName: string]: { [field: stri
 		if (hasInvalidField) invalidFields.push(hasInvalidField);
 	});
 
+	console.log({ invalidFields });
 	return invalidFields;
 };
