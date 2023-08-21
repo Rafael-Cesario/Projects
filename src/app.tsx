b411d1ffb8f54b0430c3df1a3473e2306f85b6f6
Rafael <rafaeloliveiracds@gmail.com>
2023-08-21 16:34:36 -0300

    Project reset and initial configs

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+const App = () => {
+	return <StyledTitle>Agile Quiz</StyledTitle>;
+};
+
+const StyledTitle = styled.h1`
+	background-color: blue;
+	color: #ddd;
+`;
+
+export default App;
